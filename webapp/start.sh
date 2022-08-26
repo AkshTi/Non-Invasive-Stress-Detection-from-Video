73d11e1beae23e61994ba671d2ce71d8357a6e8d
Daniel Haehn <haehn@mpsych.org>
2022-08-25 22:00:58 -0400

    Add real ssl certificate.

--- webapp/start.sh
+++ webapp/start.sh
@@ -1,3 +1,3 @@
-export LD_LIBRARY_PATH=/home/d/miniconda3/envs/STRESSGPU3/lib/
+export LD_LIBRARY_PATH=/home/d/miniconda3/envs/STRESSGPU/lib/
 
-flask run --host=0.0.0.0 --port=45678 --cert=adhoc
+flask run --host=0.0.0.0 --port=45678 --cert=../../am.i.verymad.pem --key=../../private.key
